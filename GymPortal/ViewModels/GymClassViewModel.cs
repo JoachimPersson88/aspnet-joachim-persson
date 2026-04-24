2889aeb607d663ff891e6519bb2a3368f3090fb4
JoachimPersson88 <joachim.persson88@gmail.com>
2026-04-24 15:01:01 +0200

    "GymClass START" Skapat view model för gymclass

---
+++ GymPortal/ViewModels/GymClassViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GymPortal.Web.ViewModels;
+
+public class GymClassViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string? Instructor { get; set; }
+    public DateTime StartTime { get; set; }
+    public int Capacity { get; set; }
+}
