afb445d7f0d3cfbb90fa6d1efe0a059229eb3be9
JoachimPersson88 <joachim.persson88@gmail.com>
2026-04-28 12:41:42 +0200

    "MyPage START" Skapat View model för Min sida

---
+++ GymPortal/ViewModels/MyPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GymPortal.Web.ViewModels;
+
+public class MyPageViewModel
+{
+    public ProfileViewModel Profile { get; set; } = new();
+    public MembershipViewModel? Membership { get; set; }
+    public List<BookingViewModel> Bookings { get; set; } = new();
+}
